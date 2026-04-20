bb0e2a66ae1944e935c521f700a1fb6c5a545f6f
sysco-k-sakaguchi <146063012+sysco-k-sakaguchi@users.noreply.github.com>
2026-04-20 18:35:38 +0900

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,26 @@
+import { EXPERIENCE_CONFIG, EXPERIENCE_SETTINGS, PLACEHOLDER_URL } from "./config.js";
+import { bindContent, setupPlaceholderLinks } from "./modules/content.js";
+import { setupCurtain } from "./modules/curtain.js";
+import { setupPhotoGallery } from "./modules/photo-gallery.js";
+import { setupRevealObserver } from "./modules/reveal.js";
+
+function initializeExperience() {
+  bindContent(EXPERIENCE_CONFIG);
+  setupPlaceholderLinks({
+    placeholderValue: PLACEHOLDER_URL
+  });
+
+  const revealController = setupRevealObserver();
+  revealController.observeAll(document.querySelectorAll("[data-reveal]"));
+
+  setupPhotoGallery();
+
+  setupCurtain({
+    introDelay: EXPERIENCE_SETTINGS.curtainIntroDelay,
+    preludeDuration: EXPERIENCE_SETTINGS.curtainPreludeDuration,
+    openDuration: EXPERIENCE_SETTINGS.curtainOpenDuration,
+    revealDelay: EXPERIENCE_SETTINGS.curtainRevealDelay
+  });
+}
+
+initializeExperience();
